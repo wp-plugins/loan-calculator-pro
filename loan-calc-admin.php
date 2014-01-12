644b3444f247755455b5d0871ebacf691f520ee5
lightimagemedia <lightimagemedia@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-12 08:53:04 +0000

    uploading files
    git-svn-id: https://plugins.svn.wordpress.org/loan-calculator-pro/trunk@837017 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ loan-calc-admin.php
@@ -0,0 +1,19 @@
+<div class="loan_calc_admin_main" style="margin:20px 0px">
+	<div class="updated">
+		<p>Add this short code
+			<strong>[loan-calculator]</strong>
+			to your page.where you want to show the loan calculator.
+		</p>
+	</div>
+	<div class="updated" style="margin-bottom:20px">
+		<p>You can also use it as widget.Please check the widget section on the wordpress <strong>appearance tabs </strong></p>
+	</div>
+    <center><img src="http://lightimagemedia.com/wp-content/uploads/2014/01/loancalculator.png"/></center>
+		<center> Created By</center>
+        
+        <center>
+	<img src="http://lightimagemedia.com/wp-content/uploads/2013/04/logo2.png"/></center>
+	<center>
+	  <a href="https://www.lightimagemedia.com/">Visit Us - Light Image Media</a>
+	</center>
+</div>
